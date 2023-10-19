ba06b048c214ba28196757e35593a8ed19707373
Jan Kapellen <jan.kapellen@stroeer.de>
2023-10-19 10:46:21 +0200

    added an optional variable to define a greeting target

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "greeting" {
-  value       = "Hello World!"
+  value       = "Hello ${length(var.greeting_target) > 0 ? title(var.greeting_target) : "World"}!"
   description = "Says hello to the world via Terraform"
 }
