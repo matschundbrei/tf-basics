024b56722e94fd1a01b87c3fbb98e6337153ec83
Jan Kapellen <jan.kapellen@stroeer.de>
2023-10-19 10:47:27 +0200

    description of output corrected

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
 output "greeting" {
   value       = "Hello ${length(var.greeting_target) > 0 ? title(var.greeting_target) : "World"}!"
-  description = "Says hello to the world via Terraform"
+  description = "Says hello to the World or a defined greeting_target via Terraform"
 }
